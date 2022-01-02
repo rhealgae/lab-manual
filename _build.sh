4736d6a0008ed4d550d990fe05499cb80615119a
Ryan <ryanstraight@arizona.edu>
2022-01-02 16:30:48 -0700

    Removing HTML and NUL with bash script

--- _build.sh
+++ _build.sh
@@ -7,3 +7,9 @@ Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
 RScript -e "rmarkdown::render('_README.Rmd', output_format = 'github_document',
                   output_file = paste('README.md'))"
+
+# Remove the HTML file produced by a preview
+rm -r README.html
+
+# Remove the nul file produced with PDF creation
+rm -r nul
